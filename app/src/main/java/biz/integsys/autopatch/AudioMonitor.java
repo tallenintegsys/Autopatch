e981bf59a12d2558f94319ddb3c87cbed9d75d0c
tallenintegsys <tallen@integsys.biz>
2015-12-11 22:14:06 -0500

    Its working but hte "bins" are off by about 25

--- app/src/main/java/biz/integsys/autopatch/AudioMonitor.java
+++ app/src/main/java/biz/integsys/autopatch/AudioMonitor.java
@@ -13,21 +13,39 @@ public class AudioMonitor {
     private final AudioRecord audioRecord;
     private Thread monitorThread;
     private final float[] recordBuffer = new float[44100];
+    private final double[] re = new double[32768];
+    private double[] im = new double[32768];
+    private final double[] zero = new double[32768];
+    private final FFT fft = new FFT(32768);
+    private boolean enable;
 
     public AudioMonitor() {
         audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, 44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_FLOAT, 44100);
         ///XXX need to check that permissions were okay etc...
         int state = audioRecord.getState();
-        Log.d("AudioMonitor", "state: "+ state);
+        Log.d("AudioMonitor", "state: " + state);
+        for (int i=0; i < im.length; i++)
+            zero[i]=0; //do i actually need to do this?
     }
 
     public void start() {
+        enable = true;
         audioRecord.startRecording();
         monitorThread = new Thread(new Runnable() {
             public void run() {
-                while (!monitorThread.isInterrupted()) {
+                do {
                     int read = audioRecord.read(recordBuffer, 0, 44100, AudioRecord.READ_BLOCKING);
                     Log.d("AudioMonitor", "read " + read + " floats.");
+                    im = zero.clone(); //memset, I hope?
+                    for (int i=0; i < 32768; i++)
+                        re[i] = recordBuffer[i];
+                    fft.fft(re, im);
+                    //Log.d("AudioMonitor", "FFT done.");
+                } while (enable);
+                for (int i=0; i < 32768; i++) {
+                    if ((Math.abs(im[i]) > 1000) || (Math.abs(re[i]) > 1000))
+                        Log.v("AudioMonitor", "i="+i+"   x="+ im[i]+"   y="+ re[i]);
+                    audioRecord.stop();
                 }
             }
         });
@@ -35,7 +53,7 @@ public class AudioMonitor {
     }
 
     public void stop() {
-        monitorThread.interrupt();
-        audioRecord.stop();
+        //monitorThread.interrupt();
+        enable = false;
     }
 }
