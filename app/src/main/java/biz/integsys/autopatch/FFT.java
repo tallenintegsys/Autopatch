db652a8babb6262ed748d3e9268e8ed7399c516d
tallenintegsys <tallen@integsys.biz>
2015-12-13 16:23:09 -0500

    I really need to "templateize" FFT and test it better

--- app/src/main/java/biz/integsys/autopatch/FFT.java
+++ app/src/main/java/biz/integsys/autopatch/FFT.java
@@ -31,8 +31,8 @@ class FFT {
     private final int m;
 
     // Lookup tables. Only need to recompute when size of FFT changes.
-    private double[] cos;
-    private double[] sin;
+    private double[] cos, sin;
+    private float[] cosf, sinf;
 
     public FFT(int n) {
         this.n = n;
@@ -45,10 +45,14 @@ class FFT {
         // precompute tables
         cos = new double[n / 2];
         sin = new double[n / 2];
+        cosf = new float[n / 2];
+        sinf = new float[n / 2];
 
         for (int i = 0; i < n / 2; i++) {
             cos[i] = Math.cos(-2 * Math.PI * i / n);
             sin[i] = Math.sin(-2 * Math.PI * i / n);
+            cosf[i] = (float)Math.cos(-2 * Math.PI * i / n);
+            sinf[i] = (float)Math.sin(-2 * Math.PI * i / n);
         }
 
     }
@@ -90,6 +94,57 @@ class FFT {
             for (j = 0; j < n1; j++) {
                 c = cos[a];
                 s = sin[a];
+                a += 1 << (m - i - 1);
+
+                for (k = j; k < n; k = k + n2) {
+                    t1 = c * x[k + n1] - s * y[k + n1];
+                    t2 = s * x[k + n1] + c * y[k + n1];
+                    x[k + n1] = x[k] - t1;
+                    y[k + n1] = y[k] - t2;
+                    x[k] = x[k] + t1;
+                    y[k] = y[k] + t2;
+                }
+            }
+        }
+    }
+
+    public void fft(float[] x, float[] y) {
+        int i, j, k, n1, n2, a;
+        float c, s, t1, t2;
+
+        // Bit-reverse
+        j = 0;
+        n2 = n / 2;
+        for (i = 1; i < n - 1; i++) {
+            n1 = n2;
+            while (j >= n1) {
+                j = j - n1;
+                n1 = n1 / 2;
+            }
+            j = j + n1;
+
+            if (i < j) {
+                t1 = x[i];
+                x[i] = x[j];
+                x[j] = t1;
+                t1 = y[i];
+                y[i] = y[j];
+                y[j] = t1;
+            }
+        }
+
+        // FFT
+        n1 = 0;
+        n2 = 1;
+
+        for (i = 0; i < m; i++) {
+            n1 = n2;
+            n2 = n2 + n2;
+            a = 0;
+
+            for (j = 0; j < n1; j++) {
+                c = (float) cosf[a];
+                s = (float) sinf[a];
                 a += 1 << (m - i - 1);
 
                 for (k = j; k < n; k = k + n2) {
