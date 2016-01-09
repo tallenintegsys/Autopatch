583b92ecdfc5e58df7ed339473eb7dcf4659e50c
tallenintegsys <tallen@integsys.biz>
2016-01-09 11:47:44 -0500

    roughing in a test class to sort our the anomalies with FFT

---
+++ app/src/test/java/biz/integsys/autopatch/FFTTest.java
@@ -0,0 +1,16 @@
+package biz.integsys.autopatch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jshoop on 1/9/16.
+ */
+public class FFTTest {
+
+    @Test
+    public void testFft() throws Exception {
+
+    }
+}
