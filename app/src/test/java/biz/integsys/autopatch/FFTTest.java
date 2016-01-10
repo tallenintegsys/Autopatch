3655925d2db3b00e6ac84c0c4e3e3137f0ea7110
tallenintegsys <tallen@integsys.biz>
2016-01-10 10:18:43 -0500

    I beginnings of a test for the FFT

--- app/src/test/java/biz/integsys/autopatch/FFTTest.java
+++ app/src/test/java/biz/integsys/autopatch/FFTTest.java
@@ -1,5 +1,9 @@
 package biz.integsys.autopatch;
 
+import android.util.Log;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -9,8 +13,30 @@ import static org.junit.Assert.*;
  */
 public class FFTTest {
 
-    @Test
+    final int SAMPLESIZE = 32768;
+    final int SAMPLERATE = 44100;
+    private final float[] re = new float[SAMPLESIZE];
+    private float[] im = new float[SAMPLESIZE];
+    private final FFT Fft = new FFT(SAMPLESIZE);
+
+    @Before
+    public void setUp() throws Exception {
+        for (int i=0; i < SAMPLESIZE; i++) {
+            re[i] = (float)Math.sin(440 * i / SAMPLERATE);
+            im[i] = 0;
+        }
+    }
+
+        @Test
     public void testFft() throws Exception {
+        Fft.fft(re, im);
+        System.out.print("testFft");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    for (int i=0; i < SAMPLESIZE; i++)
+        System.out.print("i: " + i + "   re: " + re[i] + "   im: " + im[i]+"\n");
 
     }
 }
